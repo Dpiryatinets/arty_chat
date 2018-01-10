f030b146787ce511ae83f93a26f18f1aaedbe484
akhabibullin <noangell@bk.ru>
2018-01-10 12:16:15 +0300

    add homework

--- operations.js
+++ operations.js
@@ -1,9 +1,21 @@
 'use strict';
 
 function sum(a, b) {
-  return a + b;
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mult(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
 }
 
 module.exports = {
-    sum,
+    sum, sub, mult, div,
 };
