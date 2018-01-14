a997cac6a84bb853a33263d8ea1b5f969c57fe7c
Dmitry Piryatinets <dmitry.piryatinets@onetwotrip.com>
2018-01-14 16:43:47 +0300

    map/reduce homework

---
+++ homework.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const ozzyText = {
+    a: 'I ',
+    b: 'don\'t want ',
+    c: 'to ',
+    d: 'the ',
+    f: 1,
+    e: 'world'
+};
+
+const mapResult = Object.keys(ozzyText)
+    .map(getText);
+function getText(key) {
+    return ozzyText[key];
+}
+console.log(mapResult);
+// Задание номер 1
+// Преобразовать массив свойств объекта ozzyText так,
+// чтобы в результирующем массиве оказались только строковые значения
+
+const reduceResult = Object.keys(ozzyText)
+    .reduce(function(accumulator, currentKey){
+        const currentValue = ozzyText[currentKey];
+        return accumulator + currentValue;
+    }, '');
+console.log(reduceResult);
+
+// Задание номер 2
+// С помощью reduce и Object.keys написать функцию invert,
+// которая принимает в себя объект и меняет у него ключи и значения свойств местами
+const input = { a: 'b', c: 'd' };
+const output = invert(input);
+console.log(output); // { b: 'a', d: 'c' }
