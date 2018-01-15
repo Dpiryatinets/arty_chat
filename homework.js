00a1c7025b27edc9060a5e75edb386eec56847f7
akhabibullin <noangell@bk.ru>
2018-01-15 22:13:38 +0300

    The task No. 1 is performed

--- homework.js
+++ homework.js
@@ -5,7 +5,7 @@ const ozzyText = {
     b: 'don\'t want ',
     c: 'to ',
     d: 'the ',
-    f: 1,
+    f: 165,
     e: 'world'
 };
 
@@ -14,21 +14,27 @@ const mapResult = Object.keys(ozzyText)
 function getText(key) {
     return ozzyText[key];
 }
-console.log(mapResult);
+const joint = mapResult.join('');
+const finalResult = joint.replace(/[0-9]/g, '');
+console.log(finalResult);
 // Задание номер 1
 // Преобразовать массив свойств объекта ozzyText так,
 // чтобы в результирующем массиве оказались только строковые значения
 
+/*
 const reduceResult = Object.keys(ozzyText)
     .reduce(function(accumulator, currentKey){
         const currentValue = ozzyText[currentKey];
-        return accumulator + currentValue;
-    }, '');
+       return accumulator + currentValue;
+   }, '');
 console.log(reduceResult);
+*/
 
 // Задание номер 2
 // С помощью reduce и Object.keys написать функцию invert,
 // которая принимает в себя объект и меняет у него ключи и значения свойств местами
+/*
 const input = { a: 'b', c: 'd' };
 const output = invert(input);
 console.log(output); // { b: 'a', d: 'c' }
+*/
